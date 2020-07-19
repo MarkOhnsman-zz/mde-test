046caeb18e06e3beaacc17d32fdcf0fd558866bc
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2020-07-19 01:52:47 -0600

    Review Markdown Editor

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <h1>Welcome to Your Vue.js App</h1>
+  <div>
+    <h1>Toast-Ui/Vue-Editor</h1>
+    <a href="https://github.com/nhn/tui.editor/tree/master/apps/vue-editor">Source Here</a>
+    <editor ref="toastuiEditor" initialEditType="wysiwyg" />
+    <button @click="getHtml">Get HTML (see console)</button>
+    <button @click="getMD">Get Markdown (see console)</button>
+    <h3>MD Viewer</h3>
+    <viewer
+      style="border: 1px solid black; padding: 1em;"
+      ref="toastuiEditor2"
+      :initialValue="preview"
+    />
   </div>
 </template>
+<script>
+import 'codemirror/lib/codemirror.css';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import '@toast-ui/editor/dist/toastui-editor-viewer.css';
+
+import { Viewer } from '@toast-ui/vue-editor';
+import { Editor } from '@toast-ui/vue-editor';
+
+export default {
+  data() {
+    return {
+      editorOptions: {
+        toolBarItems: []
+      },
+      preview: 'Use this to ***Render*** the Markdown'
+    }
+  },
+  components: {
+    Editor,
+    Viewer
+  },
+  methods: {
+    getHtml() {
+      let html = this.$refs.toastuiEditor.invoke('getHtml');
+      console.log(html)
+    },
+    getMD() {
+      let md = this.$refs.toastuiEditor.invoke('getMarkdown');
+      console.log(md)
+    }
+  }
+};
+</script>
